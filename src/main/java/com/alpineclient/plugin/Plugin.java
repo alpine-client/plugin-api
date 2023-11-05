ebab78273ab84336ab0b1cec3577f5d3c8edc420
Thomas <tomwmth@pm.me>
2023-11-05 11:52:07 +0800

    Replace Reflections with Guava

--- src/main/java/com/alpineclient/plugin/Plugin.java
+++ src/main/java/com/alpineclient/plugin/Plugin.java
@@ -7,18 +7,20 @@ import com.alpineclient.plugin.framework.Command;
 import com.alpineclient.plugin.framework.EventListener;
 import com.alpineclient.plugin.framework.PluginListener;
 import com.alpineclient.plugin.network.NetHandlerPlugin;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.reflect.ClassPath;
 import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.ApiStatus;
-import org.reflections.Reflections;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
-import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author Thomas Wearmouth
@@ -52,75 +54,53 @@ public final class Plugin extends JavaPlugin {
         this.netHandler = new NetHandlerPlugin();
 
         this.setupLocales();
-        this.registerEventListeners();
-        this.registerPluginListeners();
-        this.registerCommands();
+        this.registerAll();
     }
 
-    private void registerEventListeners() {
-        Reflections reflections = new Reflections("com.alpineclient.plugin.listener.event");
-        Set<Class<? extends EventListener>> classes = reflections.getSubTypesOf(EventListener.class);
-
-        // Register event listeners
-        for (Class<? extends EventListener> clazz : classes) {
+    @SuppressWarnings("UnstableApiUsage")
+    private void registerAll() {
+        String packageName = this.getClass().getPackage().getName();
+        Set<Class<?>> clazzes = ImmutableSet.of();
+        try {
+            clazzes = ClassPath.from(this.getClassLoader()).getAllClasses().stream()
+                    .filter(clazz -> clazz.getPackageName().contains(packageName))
+                    .map(ClassPath.ClassInfo::load)
+                    .collect(Collectors.toSet());
+        }
+        catch (Exception ex) {
+            Reference.LOGGER.error("Error scanning classpath", ex);
+        }
+        for (Class<?> clazz : clazzes) {
             if (Modifier.isAbstract(clazz.getModifiers()))
                 continue;
 
             try {
-                EventListener event = clazz.getDeclaredConstructor().newInstance();
-                this.getServer().getPluginManager().registerEvents(event, this);
+                if (EventListener.class.isAssignableFrom(clazz)) {
+                    Constructor<? extends EventListener> constructor = ((Class<? extends EventListener>) clazz).getDeclaredConstructor();
+                    constructor.setAccessible(true);
+                    EventListener listener = constructor.newInstance();
+                    this.getServer().getPluginManager().registerEvents(listener, this);
+                }
+                else if (PluginListener.class.isAssignableFrom(clazz)) {
+                    Constructor<? extends PluginListener> constructor = ((Class<? extends PluginListener>) clazz).getDeclaredConstructor();
+                    constructor.setAccessible(true);
+                    PluginListener listener = constructor.newInstance();
+                    Bukkit.getMessenger().registerOutgoingPluginChannel(this, listener.getChannelId());
+                    Bukkit.getMessenger().registerIncomingPluginChannel(this, listener.getChannelId(), listener);
+                }
+                else if (Command.class.isAssignableFrom(clazz)) {
+                    Constructor<? extends Command> constructor = ((Class<? extends Command>) clazz).getDeclaredConstructor();
+                    constructor.setAccessible(true);
+                    Command command = constructor.newInstance();
+                    this.commandManager.registerCommand(command, true);
+                    command.registerCompletions(this.commandManager);
+                    command.registerConditions(this.commandManager);
+                }
             }
             catch (Exception ex) {
-                Reference.LOGGER.error("Unable to register event listener {}", clazz.getName());
+                Reference.LOGGER.error("Failed to register " + clazz.getName(), ex);
             }
         }
-    }
-
-    private void registerPluginListeners() {
-        Reflections reflections = new Reflections("com.alpineclient.plugin.listener.plugin");
-        Set<Class<? extends PluginListener>> classes = reflections.getSubTypesOf(PluginListener.class);
-
-        // Register plugin listeners
-        for (Class<? extends PluginListener> clazz : classes) {
-            if (Modifier.isAbstract(clazz.getModifiers()))
-                continue;
-
-            try {
-                PluginListener plugin = clazz.getDeclaredConstructor().newInstance();
-                Bukkit.getMessenger().registerOutgoingPluginChannel(this, plugin.getChannelId());
-                Bukkit.getMessenger().registerIncomingPluginChannel(this, plugin.getChannelId(), plugin);
-            }
-            catch (Exception ex) {
-                Reference.LOGGER.error("Unable to register plugin listener {}", clazz.getName());
-            }
-        }
-    }
-
-    private void registerCommands() {
-        Reflections reflections = new Reflections("com.alpineclient.plugin.command");
-        Set<Class<? extends Command>> classes = reflections.getSubTypesOf(Command.class);
-        Set<Command> commands = new HashSet<>();
-
-        // Register commands
-        for (Class<? extends Command> clazz : classes) {
-            if (Modifier.isAbstract(clazz.getModifiers()))
-                continue;
-
-            try {
-                Command command = clazz.getDeclaredConstructor().newInstance();
-                this.commandManager.registerCommand(command, true);
-                commands.add(command);
-            }
-            catch (Exception ex) {
-                Reference.LOGGER.error("Unable to register command {}", clazz.getName());
-            }
-        }
-
-        // Register completions and conditions
-        commands.forEach(cmd -> {
-            cmd.registerCompletions(commandManager);
-            cmd.registerConditions(commandManager);
-        });
     }
 
     private void setupLocales() {
