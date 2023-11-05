282781c18a3782e1fb0224bec1458bc1fd6d8c5f
Thomas <tomwmth@pm.me>
2023-11-05 12:44:14 +0800

    Update publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import java.text.SimpleDateFormat
+import java.util.*
 
 plugins {
-    id("java")
+    id("java-library")
     id("maven-publish")
     id("net.kyori.blossom") version "1.2.0"
     id("com.github.johnrengelman.shadow") version "7.1.2"
@@ -29,15 +31,15 @@ dependencies {
     compileOnly("org.apache.logging.log4j:log4j-core:2.0-beta9")
     compileOnly("org.spigotmc:spigot-api:${project.properties["spigot_version"]}")
 
-    shade(this, "org.jetbrains:annotations:24.0.1")
-    shade(this, "co.aikar:acf-paper:0.5.1-SNAPSHOT")
-    shade(this, "de.exlll:configlib-spigot:4.2.0")
+    depend(this, "org.jetbrains:annotations:24.0.1", true)
+    depend(this, "co.aikar:acf-paper:0.5.1-SNAPSHOT")
+    depend(this, "de.exlll:configlib-spigot:4.2.0")
 
-    shade(this, "net.kyori:adventure-platform-bukkit:4.3.0")
+    depend(this, "net.kyori:adventure-platform-bukkit:4.3.0")
     val adventureVer = "4.14.0"
-    shade(this, "net.kyori:adventure-api:${adventureVer}")
-    shade(this, "net.kyori:adventure-text-minimessage:${adventureVer}")
-    shade(this, "net.kyori:adventure-text-serializer-legacy:${adventureVer}")
+    depend(this, "net.kyori:adventure-api:${adventureVer}")
+    depend(this, "net.kyori:adventure-text-minimessage:${adventureVer}")
+    depend(this, "net.kyori:adventure-text-serializer-legacy:${adventureVer}")
 
     val lombok = "org.projectlombok:lombok:1.18.30"
     compileOnly(lombok)
@@ -58,6 +60,24 @@ tasks.withType<Jar> {
     exclude("META-INF/versions/")
     exclude("META-INF/maven/")
     exclude("javassist/**/*.html")
+
+    // Fill out manifest
+    manifest {
+        attributes(
+            "Manifest-Version" to "1.0",
+            "Created-By" to "Gradle",
+            "Built-JDK" to "${System.getProperty("java.version")} (${System.getProperty("java.vendor")})",
+            "Built-By" to System.getProperty("user.name"),
+            "Built-Date" to SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").format(Date()),
+            "Name" to project.group.toString().replace(".", "/"),
+            "Implementation-Title" to project.properties["plugin_name"],
+            "Implementation-Version" to project.version,
+            "Implementation-Vendor" to "Crystal Development, LLC.",
+            "Specification-Title" to project.properties["plugin_name"],
+            "Specification-Version" to project.version,
+            "Specification-Vendor" to "Crystal Development, LLC.",
+        )
+    }
 }
 
 tasks.withType<ShadowJar> {
@@ -95,7 +115,8 @@ tasks.processResources {
 publishing {
     publications {
         create<MavenPublication>("maven") {
-            from(components["java"])
+            artifact(tasks["jar"])
+
             pom {
                 name.set(project.properties["plugin_name"] as String)
                 description.set("Plugin implementation of the Alpine Client API.")
@@ -105,6 +126,18 @@ publishing {
                 artifactId = "api-plugin"
                 version = compileVersion()
                 packaging = "jar"
+
+                withXml {
+                    val dependenciesNode = asNode().appendNode("dependencies")
+
+                    configurations["api"].allDependencies.forEach {
+                        val dependencyNode = dependenciesNode.appendNode("dependency")
+                        dependencyNode.appendNode("groupId", it.group)
+                        dependencyNode.appendNode("artifactId", it.name)
+                        dependencyNode.appendNode("version", it.version)
+                        dependencyNode.appendNode("scope", "compile")
+                    }
+                }
             }
         }
     }
@@ -141,7 +174,9 @@ fun compileVersion(): String {
     return "${major}.${minor}.${patch}${if (preRelease == "none") "" else preRelease}"
 }
 
-fun shade(scope: DependencyHandlerScope, dependency: String) {
+fun depend(scope: DependencyHandlerScope, dependency: String, api: Boolean = false) {
     scope.implementation(dependency)
     scope.shadow(dependency)
+    if (api)
+        scope.api(dependency)
 }
